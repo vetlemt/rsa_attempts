d16e80b8876f5e78a4d5cd43bac55bb9986a661c
vetlemt <vetle24@gmail.com>
2019-09-26 12:13:54 +0200

    please work

--- rsa.c
+++ rsa.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <math.h>
-int M = 4;
-int n = 3;
-int e = 2;
+int M = 10;
+int n = 17;
+int e = 5;
 long exponent = 1;
-long exp_M = 0;
 long result = 1;
-long mod_exp_M = 0;
 
 int main(){
+printf("%d^%d mod %d = \n",M,e,n);
+	M %= n;
 	for (int i = 0; i < 32; i++){
-		if ((e>>i) && 1 == 1){
-			mod_exp_M = M % n;
-			result *= mod_exp_M;
+		if (e >> i & 1 == 1 ){
+			result *= M % n;
+			printf("%ld\n",result);
 		}
 	M *= M % n;	
-	exponent = 1 << i;
 	}
 //long true_result = pow(M,e);
 //true_result %= n;
 
-printf("my rsa: %ld \n%d^%d mod %d \n",result,M,e,n);
+printf("my rsa: %ld \n",result);
 return 0;
 }
