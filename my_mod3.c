1f8357466535c81ab39de4f3177990d1b46d092d
vetlemt <55287185+vetlemt@users.noreply.github.com>
2019-10-03 13:59:50 +0200

    added new alternative version

---
+++ my_mod3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+long my_mod(long B, long n){
+    if (n == 0){return B;} 
+    long carry = B;
+    long carry_nxt = carry;
+    long k = 1;
+    bool finished = false;
+    while (!finished){ 
+        k < 1 ? k = 1 : 0;
+        carry_nxt = carry - (n << k);
+        if (carry_nxt < 0){
+            carry = carry;
+            k--;
+        }
+        else{
+            carry_nxt < n ? finished = true : 0;
+            carry = carry_nxt;
+            k++;
+        }
+        printf("C(i) = %ld, C(i+1) = %ld, k = %ld \n", carry_nxt, carry, k);
+    }
+return carry;
+}
+
+
+void main(){
+    long B = 177;
+    long n = 6;
+    long ans = my_mod(B,n);
+    printf("my: %ld mod %ld = %ld\n", B, n, ans);
+    printf("C: %ld mod %ld = %ld\n", B, n, B%n);
+}
+
