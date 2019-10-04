fc54dbe3dd2ef78495410f7e9aa124c709bad901
vetlemt <55287185+vetlemt@users.noreply.github.com>
2019-10-04 17:28:15 +0200

    the code has to use signed numbers

--- my_mod3.c
+++ my_mod3.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-long my_mod(__uint128_t B, __uint128_t n){
+long my_mod(__int128_t B, __int128_t n){
     if (n == 0){return B;} 
     int i = 0;
-    __uint128_t carry = B;
-    __uint128_t carry_nxt = carry;
-    __uint128_t k = 1;
+    __int128_t carry = B;
+    __int128_t carry_nxt = carry;
+    __int128_t k = 1;
     bool finished = false;
     while (!finished){ 
         k < 0 ? k = 0 : 0;
@@ -28,9 +28,9 @@ return carry;
 
 
 void main(){
-    __uint128_t B = -1;
-    __uint128_t n = 3;
-    __uint128_t ans = my_mod(B,n);
+    __int128_t B = -1 ^ (1<<127);
+    __int128_t n = 3;
+    __int128_t ans = my_mod(B,n);
     printf("my: %g mod %g = %g\n", (double)B, (double)n, (double)ans);
     printf(" C: %g mod %g = %g\n", (double)B, (double)n, (double)(B%n));
 }
