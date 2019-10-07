4a4f47c8ba08d22591161dda5f443be9ee908214
vetlemt <vetle24@gmail.com>
2019-10-07 17:24:30 +0200

    ulitmate form complete

--- my_mod4.c
+++ my_mod4.c
@@ -28,16 +28,10 @@ return carry;
 
 
 void main(){
-<<<<<<< HEAD
     long B = 9e18;
-    B+= 1;
+    B+= 7;
     long n = 3;
     printf("msbSize(B) = %d\n",msbSize(B));
-=======
-    long B = 2e7+1;
-    long n = 3;
-    printf("msbSize(9) = %d\n",msbSize(9));
->>>>>>> aa3eca8427d06084a6c1a994686ffb64eaba07e5
     long ans = my_mod(B,n);
     printf("my: %ld mod %ld = %ld\n", B, n, ans);
     printf("C: %ld mod %ld = %ld\n", B, n, B%n);
