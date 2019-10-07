71fbe8eace15a4b723400b1b210bd5f866068809
vetlemt <55287185+vetlemt@users.noreply.github.com>
2019-10-07 17:34:51 +0200

    128bit

--- my_mod4.c
+++ my_mod4.c
@@ -3,37 +3,36 @@
 #include <math.h>
 #include <stdlib.h>
 
-int msbSize(long a){
+int msbSize(__uint128_t a){
     return (int)log2(a);
 }
 
-long my_mod(long B, long n){
+long my_mod(__uint128_t carry, __uint128_t n){
     if (n == 0){return B;} 
-    long carry = B;
-    long k = 0;
-    bool finished = false;
+    int k = 0;
     int i = 0;
     int kc = msbSize(n) + 1;
+    bool finished = false;
     while (!finished){ 
-	k = msbSize(carry) - kc;
-	k < 0 ? k = 0 : 0;
+	    k = msbSize(carry) - kc;
+	    k < 0 ? k = 0 : 0;
         carry -= ( n << k );
         carry < n ? finished = true : 0;
       
-        printf("C(%d) = %ld,  k = %ld \n",i, carry, k);
-	i++;
+        printf("C(%d) = %g,  k = %d \n",i, (float)carry, k);
+	    i++;
     }
 return carry;
 }
 
 
 void main(){
-    long B = 9e18;
+    __uint128_t B = 3e38;
     B+= 7;
-    long n = 3;
+    __uint128_t n = 3;
     printf("msbSize(B) = %d\n",msbSize(B));
-    long ans = my_mod(B,n);
-    printf("my: %ld mod %ld = %ld\n", B, n, ans);
-    printf("C: %ld mod %ld = %ld\n", B, n, B%n);
+    __uint128_t ans = my_mod(B,n);
+    printf("my: %g mod %g = %g\n", (float)B, (float)n, (float)ans);
+    printf(" C: %g mod %g = %g\n", (float)B, (float)n, (float)(B%n));
 }
 
