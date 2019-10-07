e1d36660aa5c92d561175aa09736d9b39d3dcfd1
vetlemt <vetle24@gmail.com>
2019-10-07 17:12:11 +0200

    almighty mod algo complete

--- my_mod4.c
+++ my_mod4.c
@@ -4,46 +4,34 @@
 #include <stdlib.h>
 
 int msbSize(long a){
-    int b = abs(a);
-    int i = 0;
-    int msb = 0;
-    while(b > 0){
-        b & 1 == 1 ? msb = i : 0; 
-        b = b >> 1;
-        i++;
-    }
-    return msb;
+    return (int)log2(a);
 }
-
 
 long my_mod(long B, long n){
     if (n == 0){return B;} 
     long carry = B;
-    long carry_nxt = carry;
     long k = 0;
     bool finished = false;
+    int i = 0;
+    int kc = msbSize(n) + 1;
     while (!finished){ 
-        k < 0 ? k = 0 : 0;
-        carry_nxt = carry - ( n << k );//(n << k);
-        if (carry_nxt < 0){
-            carry = carry;
-            k -= msbSize(carry_nxt) - msbSize(n);
-        }
-        else{
-            carry_nxt < n ? finished = true : 0;
-            carry = carry_nxt;
-            k += msbSize(carry_nxt) - msbSize(n);
-        }
-        printf("C(i) = %ld, C(i+1) = %ld, k = %ld \n", carry_nxt, carry, k);
+	k = msbSize(carry) - kc;
+	k < 0 ? k = 0 : 0;
+        carry -= ( n << k );
+        carry < n ? finished = true : 0;
+      
+        printf("C(%d) = %ld,  k = %ld \n",i, carry, k);
+	i++;
     }
 return carry;
 }
 
 
 void main(){
-    long B = 177;
-    long n = 6;
-    printf("msbSize(9) = %d",msbSize(9))
+    long B = 9e18;
+    B+= 1;
+    long n = 3;
+    printf("msbSize(B) = %d\n",msbSize(B));
     long ans = my_mod(B,n);
     printf("my: %ld mod %ld = %ld\n", B, n, ans);
     printf("C: %ld mod %ld = %ld\n", B, n, B%n);
