0cd8f2e1bb3ade4445ea1bf9d08e4b8d19c733ac
vetlemt <55287185+vetlemt@users.noreply.github.com>
2019-11-04 20:23:46 +0100

    created mod prod and updated mod exp

--- rsa.c
+++ rsa.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 #include <math.h>
 
-int M = 7;
-int n = 15;
-int e = 13;
-long exponent = 1;
-long result = 1;
+__uint128_t modProd( __uint128_t  A,__uint128_t B,__uint128_t n){
+    __uint128_t P = 0;
+    for (int i = 0; i < 128; i++){
+        P = 2*P + A * ((B >> (128 - i)) & 1);
+        P = P % n;
+    }
+return P;
+}
 
 void main(){
-	int Me = M;
-	printf("%d^%d mod %d = \n",M,e,n);
+	__uint128_t M = 7;
+	__uint128_t n = 15;
+	__uint128_t e = 13;
+	__uint128_t exponent = 1;
+	__uint128_t result = 1;
+	__uint128_t Me = M;
+	//printf("%d^%d mod %d = \n",M,e,n);
 	M %= n;
-	for (int i = 0; i < 32; i++){
-		if (e >> i & 1 == 1 ){
-			result = (result*M) % n;
+	while ( e > 0 ){
+		if (e & 1 == 1 ){
+			result = modProd(result,M,n);
 		}
-	M *= M % n;	
+		M = modProd(M,M,n);	
+		e = e >> 1;
 	}
- long true_result = pow(Me,e);
- true_result %= n;
+	printf("Result: %f",(float)result);
+ //long true_result = pow(Me,e);
+ //true_result %= n;
 
-printf("my result: %ld \ntrue result: %ld\n The algorithm is ",result,true_result);
-result == true_result ? printf("correct\n") : printf("incorrect\n");
+//printf("my result: %ld \ntrue result: %ld\n The algorithm is ",result,true_result);
+//result == true_result ? printf("correct\n") : printf("incorrect\n");
 }
 
