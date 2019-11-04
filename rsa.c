4bc3b0ce6c928478d36e7dbb8477d9730bdf0669
vetlemt <55287185+vetlemt@users.noreply.github.com>
2019-11-04 20:24:59 +0100

    small fix to modProd

--- rsa.c
+++ rsa.c
@@ -4,7 +4,7 @@
 __uint128_t modProd( __uint128_t  A,__uint128_t B,__uint128_t n){
     __uint128_t P = 0;
     for (int i = 0; i < 128; i++){
-        P = 2*P + A * ((B >> (128 - i)) & 1);
+        P = 2*P + A * ((B >> (127 - i)) & 1);
         P = P % n;
     }
 return P;
